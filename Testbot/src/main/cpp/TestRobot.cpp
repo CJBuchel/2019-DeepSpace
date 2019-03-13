2f711c4835eea47d0fd443a691476a9ac1f7175c
CJBuchel <connorbuchel@gmail.com>
2019-03-13 19:03:27 +0800

    Updated Test Code

--- Testbot/src/main/cpp/TestRobot.cpp
+++ Testbot/src/main/cpp/TestRobot.cpp
@@ -10,37 +10,45 @@ double lastTimestamp;
 void Robot::RobotInit() {
   lastTimestamp = frc::Timer::GetFPGATimestamp();
   
-  leftMotors[0] = new curtinfrc::Talon(1);
+  leftMotors[0] = new frc::Talon(1);
   leftMotors[0]->SetInverted(false);
   left = new curtinfrc::Gearbox{ new curtinfrc::actuators::MotorVoltageController(new frc::SpeedControllerGroup(*leftMotors[0])), nullptr};
 
-  rightMotors[0] = new curtinfrc::Talon(0);
+  rightMotors[0] = new frc::Talon(0);
   rightMotors[0]->SetInverted(true);
   right = new curtinfrc::Gearbox{ new curtinfrc::actuators::MotorVoltageController(new frc::SpeedControllerGroup(*rightMotors[0])), nullptr};
 
-
-  curtinfrc::DrivetrainConfig drivetrainConfig{*left, *right};
+  curtinfrc::DrivetrainConfig drivetrainConfig{ *left, *right, &gyro, 0.58, 0.48, 0.07, 20 };
   drivetrain = new curtinfrc::Drivetrain(drivetrainConfig);
   drivetrain->SetDefault(std::make_shared<DrivetrainManualStrategy>(*drivetrain, contGroup));
   drivetrain->GetConfig().gyro->Reset();
-  drivetrain->StartLoop(100);
+  // drivetrain->StartLoop(100);
 
   Register(drivetrain);
 }
 
-void Robot::AutonomousInit() {}
-void Robot::AutonomousPeriodic() {
+void Robot::RobotPeriodic() {
   double dt = frc::Timer::GetFPGATimestamp() - lastTimestamp;
   lastTimestamp = frc::Timer::GetFPGATimestamp();
 
-  double offset = drivetrain->GetConfig().gyro->GetAngle();
-  offset += 0; // get angle from nt
-  Schedule(std::make_shared<DrivetrainAngleStrategy>(*drivetrain, curtinfrc::control::PIDGains("Drivetrain Align", 0.3, 0, 0.04), offset));
+  // drivetrain->Set(0.25, 0.25);
 
-  Update(dt);
+  drivetrain->Update(dt);
+  // Update(dt);
 }
 
-void Robot::TeleopInit() {}
+void Robot::AutonomousInit() {
+  double offset = drivetrain->GetConfig().gyro->GetAngle();
+  offset += 0; // get angle from nt
+  // Schedule(std::make_shared<DrivetrainAngleStrategy>(*drivetrain, curtinfrc::control::PIDGains("Drivetrain Align", 0.3, 0, 0.04), offset));
+}
+void Robot::AutonomousPeriodic() {
+  std::cout << tapeAngleEntry.GetDouble(0) << std::endl;
+}
+
+void Robot::TeleopInit() {
+  // Schedule(drivetrain->GetDefaultStrategy());
+}
 void Robot::TeleopPeriodic() {}
 
 void Robot::TestInit() {}
--- Testbot/src/main/include/TestRobot.h
+++ Testbot/src/main/include/TestRobot.h
@@ -3,6 +3,7 @@
 #include <frc/Timer.h>
 #include <frc/TimedRobot.h>
 #include <frc/XboxController.h>
+#include <networktables/NetworkTableInstance.h>
 #include <frc/SpeedControllerGroup.h>
 #include <frc/DoubleSolenoid.h>
 #include "controllers/CurtinControllers.h"
@@ -11,12 +12,15 @@
 #include "Gearbox.h"
 #include "Drivetrain.h"
 
+#include "sensors/NavX.h"
+
 #include "DriveStrategies.h"
 
 
 class Robot : public frc::TimedRobot, protected curtinfrc::StrategyController {
  public:
   void RobotInit() override;
+  void RobotPeriodic() override;
 
   void AutonomousInit() override;
   void AutonomousPeriodic() override;
@@ -30,7 +34,23 @@ class Robot : public frc::TimedRobot, protected curtinfrc::StrategyController {
   curtinfrc::controllers::XboxController xbox{0};
   curtinfrc::controllers::SmartControllerGroup contGroup{xbox};
 
-  curtinfrc::Talon *leftMotors[1], *rightMotors[1];
+  frc::Talon *leftMotors[1], *rightMotors[1];
   curtinfrc::Gearbox *left, *right;
+
+  curtinfrc::sensors::NavX navx{ frc::SPI::Port::kMXP, 200 };
+  curtinfrc::sensors::NavXGyro gyro{ navx.Angular(curtinfrc::sensors::AngularAxis::YAW) };
+
   curtinfrc::Drivetrain *drivetrain;
+
+
+  std::shared_ptr<nt::NetworkTable> visionTable = nt::NetworkTableInstance::GetDefault().GetTable("VisionTracking");
+  std::shared_ptr<nt::NetworkTable> hatchTable = visionTable->GetSubTable("HatchTracking");
+  std::shared_ptr<nt::NetworkTable> tapeTable = visionTable->GetSubTable("TapeTracking");
+  
+  nt::NetworkTableEntry hatchDistanceEntry  = hatchTable->GetEntry("Hatch Distance"),
+                        hatchXoffsetEntry   = hatchTable->GetEntry("Hatch X Offset"),
+                        hatchYoffsetEntry   = hatchTable->GetEntry("Hatch Y Offset"),
+                        tapeDistanceEntry   = tapeTable->GetEntry("Distance"),
+                        tapeAngleEntry      = tapeTable->GetEntry("Angle"),
+                        tapeTargetEntry     = tapeTable->GetEntry("Target");
 };
